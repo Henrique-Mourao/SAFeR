81a49f0e9be0ff3bcff4b3200c7914a933073155
henrique-mourao <henriquegmour4@gmail.com>
2025-10-18 18:10:08 -0300

    Alteração no gitattributes

--- build/generated/sources/annotationProcessor/java/main/com/devsDoAgi/SAFeR/mapper/RestricaoMapparImpl.java
+++ build/generated/sources/annotationProcessor/java/main/com/devsDoAgi/SAFeR/mapper/RestricaoMapparImpl.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-10-13T00:53:13-0300",
+    date = "2025-10-14T09:47:20-0300",
     comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.14.3.jar, environment: Java 21.0.8 (Microsoft)"
 )
 @Component
